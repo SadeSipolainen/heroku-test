d26d4e0f3e80801875965f9f81065267adafc824
Sade Sipolainen <sade.sipolainen@tuni.fi>
2021-12-07 18:08:19 +0200

    Add new project

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+var cors = require("cors");
+app.use(cors());
+
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
+
+const port = process.env.PORT || 8080;
+
+var pool = mysql.createPool(config);
+
+app.get("/", (req, res) => {
+  pool.query("SELECT * from locations", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
+});
+
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${server.address().port}`);
+});
